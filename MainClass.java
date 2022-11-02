5405b2eed545ca99cac77d47c30e4ef741478337
Kamlesh Chhipa <kamleshchhipa@Kamleshs-MacBook-Air.local>
2022-11-02 11:36:16 +0530

    polimorphisum

--- MainClass.java
+++ MainClass.java
@@ -1,12 +1,17 @@
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("let's code today in java");
+        System.out.println("let's code today in java, let's start");
+        String newline = System.lineSeparator();
+        System.out.println(newline);
+
+
         Coder p1 = new Coder();
         p1.name = "Kamlesh";
         p1.age = 23;
 
         System.out.println("This is "+p1.name+ " here and he's " + p1.age);
         p1.code();
+        p1.code(3);
     }
 }
 
@@ -17,5 +22,10 @@ class Coder {
 
     void code() {
         System.out.println("and right now " + name + " is coding in java.");
+    }
+
+    //polimorphisum - new form of code function
+    void code(int hurs) {
+        System.out.println("and "+ name + " is coding from last "+ hurs +" hours");
     }
 }
